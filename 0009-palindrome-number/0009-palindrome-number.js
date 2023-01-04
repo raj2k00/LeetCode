eb59a7af4811f775e8863e8f6ff1f951136ed0f4
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-01-04 09:59:03 +0530

    Time: 350 ms (16.28%), Space: 51.1 MB (60.64%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,8 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let stringifyNumber = String(x);
-    const reversedNumber = stringifyNumber.split("").reverse().join("");
-    if(x == reversedNumber) return true;
+    let input_number = x;
+    let reversed_number = 0;
+    let remainder = 0;
+    
+    if(x < 0) return false;
+    
+    for(; input_number != 0 ; ){
+        remainder = parseInt(input_number % 10);
+        reversed_number = reversed_number * 10 + remainder
+        input_number= parseInt(input_number / 10) 
+    }
+    if(x == reversed_number) return true;
     return false;
 };
