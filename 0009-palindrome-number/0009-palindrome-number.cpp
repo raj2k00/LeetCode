655bba1c36ea64a9831c07b7abbce2602a350a04
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-01-04 09:26:10 +0530

    Time: 22 ms (58.39%), Space: 6 MB (45.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-              long long  int d=x,m=0,z;
-		for (int i = x; i > 0; i=i/10)
-		{z=i%10;
-	     m=m*10+z;
-			
+        long long  int d = x , m=0 , z;
+		for (int i = x; i > 0; i=i/10) {z=i%10;
+	     m=m*10+z;	
 		}
-		if(d==m)
-        {
-            return true;
-        }
-        else{
-            return false;
-        } 
+		if(d==m) return true;
+        return false;
+         
     }
 };
