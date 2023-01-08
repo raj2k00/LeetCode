021d9fb05c1a5100fca1d733e135211c66418103
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-01-08 10:33:33 +0530

    Time: 7 ms (23.78%), Space: 8.9 MB (40.80%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        auto filterd_array = remove(nums.begin(), nums.end(),val);
-        return distance(nums.begin(), filterd_array);
+        auto filtered_array = remove(nums.begin(), nums.end(),val);
+        return distance(nums.begin(), filtered_array);
     }
 };
