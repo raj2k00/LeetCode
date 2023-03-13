9668083f9cc0d1155c5149bb61f46b6b83997e66
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-03-13 21:32:09 +0530

    Time: 15 ms (98.13%), Space: 19.2 MB (86.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int count = 0;
-        
+
         for(int i = 0; i < nums.size(); i++ ){
-            if(nums[i] != 0){
+            // we are just moving the non zero items to the front
+            // increasing the count to count how many items we moved
+            if(nums[i] != 0){    
                 nums[count++] = nums[i];
             }
         }
+        // adding zeros if the count is less than size of input
         while(count < nums.size()){
             nums[count++] = 0;
         }
