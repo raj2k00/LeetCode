442252567eb009f1613ffa043dcbd03c9da79a8e
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-03-16 19:48:55 +0530

    Time: 21 ms (33.65%), Space: 20 MB (49.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,46 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-//         int total = 0;
+        int total = 0;
         int n = height.size();
-//         int left[n];
-//         int right[n];
         
-//         left[0] = height[0];
-//         for(int i = 1; i < n; ++i){
-//             left[i] = max(left[i - 1], height[i]);
-//         }
-        
-//         right[n-1] = height[n-1];
-//         for(int i = i -2 ; i >= 0; --i){
-//             right[i] = max(right[i + 1], height[i]);
-//         }
-        
-//         for(int i = 0; i < n; ++i){
-//             total += min(left[i], right[i]) - height[i];
-//         }
-//         return total;
-                int left[n];
+        int left[n];
         int right[n];
         
-        long long total = 0;
-        
-        right[0] = height[0];
-        
+        // calculating left side
+        left[0] = height[0];
         for(int i = 1; i < n; ++i){
-            right[i] = max(right[i-1], height[i]); 
+            left[i] = max(left[i - 1], height[i]);
         }
         
-        
-        left[n-1] = height[n-1];
-        
+        // calculating right side
+        right[n-1] = height[n-1];
         for(int i = n-2; i >= 0; --i){
-            left[i] = max(left[i+1], height[i]);
+            right[i] = max(right[i+1], height[i]);
         }
         
         
         for(int i = 0; i < n; ++i){
-            total += min(right[i], left[i]) - height[i];
+            total += min(left[i], right[i]) - height[i];
         }
         
         return total;
