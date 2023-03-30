be6f346e5ed3057393a9ae3c0b7247906c9e0e9a
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-03-30 09:29:11 +0530

    Time: 3 ms (94.05%), Space: 14.1 MB (53.45%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -21,14 +21,14 @@ public:
 
             else if(nums[low] <= nums[mid]){              
                 // check if the target is inbetween low to mid
-                if(target >= nums[low] && nums[mid] > target){
+                if(target >= nums[low] && nums[mid] >= target){
                     high = mid - 1;
                 }else{
                      low = mid + 1;  // else search in other half
                 }    
             }else{
                 // if low to mid is not sorted, then mid to high should be sorted            
-                if(target > nums[mid] && nums[high] >= target){
+                if(target >= nums[mid] && nums[high] >= target){
                     low = mid + 1;
                 }else{
                      high = mid - 1;
