01cd88d2734045be9c57eceb0477c8ec2e9447a8
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-05 19:46:04 +0530

    Time: 3 ms (64.54%), Space: 9.2 MB (36.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,35 +2,23 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
   
-        int temp[m+n];
-                
-        int i = 0;
-        int j = 0;
-        int k = 0;
+        int i = m - 1;
+        int j = n - 1;
         
-        while(i < m && j < n){
-            if(nums1[i] < nums2[j]){
-                temp[k] = nums1[i];
-                i++;
+        int k = ( m + n ) -1;
+        
+        while(i >= 0 && j >= 0){
+            if(nums1[i]  < nums2[j]){
+                // cout << "putting " << nums2[j] << " in " << nums1[k] << endl;
+                nums1[k--] = nums2[j--];
             }else{
-                temp[k]= nums2[j];
-                j++;
+                 // cout << "setting " << nums1[i] << " in " << nums1[k] << endl;
+                nums1[k--] = nums1[i--];
             }
-            k++;
         }
         
-        while(i < m){
-            temp[k++] = nums1[i++];
-        }
-        
-        while(j < n){
-            temp[k++] = nums2[j++];
-        }
-        
-        nums1.clear();
-        
-        for(int a = 0; a < m+n; a++){
-            nums1.push_back(temp[a]);
+        while(j >= 0){
+            nums1[k--] = nums2[j--];
         }
         
     }
