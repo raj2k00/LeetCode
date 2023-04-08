4ad80d9668b7e24ea4e595a428a57b49e6578dfc
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-08 14:50:37 +0530

    Time: 4 ms (75.67%), Space: 10.8 MB (22.97%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -2,27 +2,48 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
+        // Two Pointer Approach
+//         sort(nums1.begin(), nums1.end());
+//         sort(nums2.begin(), nums2.end());
         
-        vector <int> ans;
+//         vector <int> ans;
         
-        int i = 0;
-        int j = 0;
+//         int i = 0;
+//         int j = 0;
         
-        while(i < nums1.size() && j < nums2.size()){
-            if(nums1[i] < nums2[j]){
-                i++;
-            }else if ( nums2[j] < nums1[i]){
-                j++;
-            }else{
-                ans.push_back(nums2[j]);
-                i++;
-                j++;
+//         while(i < nums1.size() && j < nums2.size()){
+//             if(nums1[i] < nums2[j]){
+//                 i++;
+//             }else if ( nums2[j] < nums1[i]){
+//                 j++;
+//             }else{
+//                 ans.push_back(nums2[j]);
+//                 i++;
+//                 j++;
+//             }
+//         }
+        
+//         return ans; 
+        
+        // Using Hash Map
+        
+            unordered_map <int, int> m;
+        
+            for(auto x: nums1){
+                m[x]++;
             }
-        }
         
-        return ans;
+            vector <int> ans;
+        
+            for(auto x : nums2){
+                if(m[x]){
+                    ans.push_back(x);
+                    m[x]--;
+                }
+            }
+        
+            return ans;
+        
         
     }
 };
