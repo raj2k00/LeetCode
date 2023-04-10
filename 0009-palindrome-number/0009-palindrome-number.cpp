e18b3a6961c46b513b13ebf0a7395059cb50a56f
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-10 19:56:55 +0530

    Time: 16 ms (59.34%), Space: 6 MB (14.57%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,19 +4,15 @@ public:
         
         if(x < 0 ) return false;
         
-        long int reverse = 0;
+        long long reverse = 0;
         int original = x;
         
-        while( x > 0 ){
-            int digit = x % 10;
-            
-            reverse = reverse * 10 + digit;
-            
+        while( x > 0 ){         
+            reverse = (reverse * 10) + (x % 10);
             x /= 10;
         }
         
         return original == reverse;
-        
         
     }
 };
