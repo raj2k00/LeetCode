ebd225a950573d28af301d0c3c90327b16b072ef
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-10 19:30:22 +0530

    Time: 3 ms (74.27%), Space: 7.8 MB (49.57%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        // sort(arr1.begin(), arr1.end()); 
         
-        int hash[1001] = {0};
-        
-        vector <int> ans;
+        vector <int> hash (1001 ,0);
         
         for(auto x: arr1){
             hash[x]++;
         }
         
+        arr1.clear();
+        
         for(int i = 0; i < arr2.size(); i++){
             while(hash[arr2[i]] != 0){
-                ans.push_back(arr2[i]);
+                arr1.push_back(arr2[i]);
                 hash[arr2[i]]--;
             }
         }
         
         for(int i = 0; i < 1001; i++){
             while(hash[i] != 0){
-                ans.push_back(i);
+                arr1.push_back(i);
                 hash[i]--;
             }
         }
         
-        return ans;
+        return arr1;
         
     }
 };
