b211194661dde4869fdca3ebdd1c310e8c12d660
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-12 18:10:33 +0530

    Time: 6 ms (11.88%), Space: 6.9 MB (94.31%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,14 +3,6 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
- 
-        
-        // transposing the matrix
-        for(int i = 0; i < n ; i++){
-            for(int j = i ; j < n; j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
         
         // Interchanging columns
         for(int i = 0; i < n; i++){
@@ -26,26 +18,7 @@ public:
             }
         }
         
-         // Interchanging columns
-        for(int i = 0; i < n; i++){
-            for(int j = 0, k = n - 1; j < k;  j++,k-- ){
-                swap(matrix[j][i], matrix[k][i]);
-            }
-        }
-        
-        // transposing the matrix
-        for(int i = 0; i < n ; i++){
-            for(int j = i ; j < n; j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-        
-        // Interchanging columns
-        for(int i = 0; i < n; i++){
-            for(int j = 0, k = n - 1; j < k;  j++,k-- ){
-                swap(matrix[j][i], matrix[k][i]);
-            }
-        }
+
         
     }
 };
