ded25593d587bbeb296a7291df44395e1c3d2c48
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-13 09:36:59 +0530

    Time: 2179 ms (5.00%), Space: 15 MB (40.35%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -2,49 +2,52 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
        
-//         int row = matrix.size();
-//         int column = matrix[0].size();
+        int row = matrix.size();
         
-//         int low = matrix[0][0];
-//         int high = matrix[row - 1][column -1];
+        if(row == 0) return false; 
+        
+        int column = matrix[0].size();
+        
+        int low = matrix[0][0];
+        int high = matrix[row - 1][column -1];
         
         
-//         if( target == low || target == high ) return true;
+        if( target == low || target == high ) return true;
         
-//         if( target < low || target > high ) return false;
+        if( target < low || target > high ) return false;
         
-//         int i = 0;
-//         int j = 0;
+        int i = 0;
+        int j = 0;
         
-//         while (i < row && j < column){
+        while (i < row && j < column){
             
-//             if(target == matrix[i][j]) {
-//                 return true;
-//             }
+            if(target == matrix[i][j]) {
+                return true;
+            }
             
-//             else if (target > matrix[i][j] && target <= matrix[i][column - 1]){
-//                 j++;
-//             }
+            else if (target > matrix[i][j] && target <= matrix[i][column - 1]){
+                j++;
+            }
             
-//             else {
-//                 j = 0;
-//                 i++;
-//             }       
+            else {
+                j = 0;
+                i++;
+            }       
             
-//         }
-            int m = matrix.size();
-    if (m == 0) return false;
-    int n = matrix[0].size();
+        }
+//             int m = matrix.size();
+//     if (m == 0) return false;
+//     int n = matrix[0].size();
 
-    int i = 0, j = n - 1;
-    while (i < m && j >= 0) {
-        if (matrix[i][j] == target)
-            return true;
-        else if (matrix[i][j] > target) {
-            j--;
-        } else 
-            i++;
-    }
+//     int i = 0, j = n - 1;
+//     while (i < m && j >= 0) {
+//         if (matrix[i][j] == target)
+//             return true;
+//         else if (matrix[i][j] > target) {
+//             j--;
+//         } else 
+//             i++;
+//     }
 
         
         return false;
