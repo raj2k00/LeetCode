bddc6a6e39f769c7bf83d82ef8915b4b4b856d9d
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-14 21:04:04 +0530

    Time: 4 ms (33.17%), Space: 6.9 MB (34.36%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         
+        // pointers to all the sides
         int top = 0;
         int bottom = matrix.size() - 1;
         int left = 0;
         int right = matrix[0].size() - 1;
         
+        // iterator
         int i = 0;
         
         vector <int> ans;
@@ -14,8 +16,9 @@ public:
         
         while(top <= bottom && left <= right){
             
-            // from left to right (first row)
+            // every for loop iteration reduces the size of the matrix;
             
+            // from left to right (first row)  
             for(i = left; i <= right; i++ ){
                 ans.push_back(matrix[top][i]);
             } 
@@ -23,14 +26,13 @@ public:
             top++;  // row completed
             
             // top to bottom (last column)
-            
             for( i = top; i <= bottom; i++){
                 ans.push_back(matrix[i][right]);
             }
             
             right--; // column completed;
             
-            
+            // right to left (last row)
             if(top <= bottom){
                 for( i = right; i >= left; i--){
                     ans.push_back(matrix[bottom][i]);
@@ -38,6 +40,7 @@ public:
                 bottom--; // row completed;
             }
             
+            // bottom to top (first column)
             if(left <= right){
                 for( i = bottom; i >=top; i--){
                     ans.push_back(matrix[i][left]);
