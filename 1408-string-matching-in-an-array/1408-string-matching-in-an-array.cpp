c3524794331224f017eafa2521def7ff9a2fcfb9
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-18 21:23:34 +0530

    Time: 3 ms (89.78%), Space: 8.5 MB (42.81%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -12,7 +12,6 @@ public:
         for(int i = words.size() - 1; i >= 0; i--){
             for(int j = words.size() - 1 ; j >= 0; j--){
                 if(words[i].find(words[j]) != string::npos && words[i] != words[j]){
-                    
                     temp.insert(words[i].length() > words[j].length() ? words[j] : words[i]);
                 }
             }
