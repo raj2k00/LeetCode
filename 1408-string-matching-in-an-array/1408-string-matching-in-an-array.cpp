097aaa33e2ee1aa04d4c65077a2061ef54980c05
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-18 21:30:39 +0530

    Time: 0 ms (100.00%), Space: 9 MB (23.32%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -4,21 +4,17 @@ public:
         
         vector <string> ans;
         
-        unordered_set <string> temp;
-        
-        sort(words.begin(), words.end());
-        
-        
-        for(int i = words.size() - 1; i >= 0; i--){
-            for(int j = words.size() - 1 ; j >= 0; j--){
-                if(words[i].find(words[j]) != string::npos && words[i] != words[j]){
-                    temp.insert(words[i].length() > words[j].length() ? words[j] : words[i]);
+        for(auto i : words){
+            for(auto j : words){
+              
+                if(i == j) continue;
+                
+                if(j.find(i) != -1){
+                    ans.push_back(i);
+                    break;
                 }
+                
             }
-        }
-        
-        for(auto i : temp){
-            ans.push_back(i);
         }
         
         return ans;
