b155194cae686bf9c04e0e9031b1f81819dea6d0
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-20 19:48:23 +0530

    Time: 10 ms (23.26%), Space: 7.5 MB (61.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,30 +2,28 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         
-        if(s.size() == 0 || s.size() == 1) return true;
+        if(s.empty()) return true;
         
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
-        
-        string alphabets = "abcdefghijklmnopqrstuvwxyz1234567890";
-        
-        string res;
-        
-        for(int i = 0; i < s.size() ; i++){
-            if(alphabets.find(s[i]) != -1){
-                res += s[i];
-            }
-        }
-        
-        if(res.size() == 0 || res.size() == 1) return true;
+        int j = s.size() - 1;
         
         int i = 0;
-        int j = res.size() - 1;
         
-        while(i <= res.size()/2 && j >= res.size()/2){
-            if(res[i] != res[j]) return false;
-            i++;
-            j--;
+        while (i < j){
+            
+            if(!isalnum(s[i])) {
+                i++;
+                continue;
+            }
+            
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            
+            if(tolower(s[i++]) != tolower(s[j--])) return false;
+            
         }
+        
         
         return true;
         
