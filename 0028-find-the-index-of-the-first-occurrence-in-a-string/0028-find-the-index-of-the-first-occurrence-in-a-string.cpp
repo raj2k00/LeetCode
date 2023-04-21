cf191e9fd45fbf3a044fad373a2e617b6a1a75dc
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-21 19:15:30 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (70.82%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,59 +1,9 @@
 class Solution {
 public:
-    
-    vector <int> failure_function (string pattern){
-        
-        int m = pattern.length();
-        
-        vector <int> res(m);
-        
-        int j = 0;
-        
-        for(int i = 1; i < m; i++ ){
-            
-            while( j > 0 && pattern[i] != pattern[j]){
-                j = res[j - 1];
-            }
-            
-            if( pattern[i] == pattern[j]){
-                cout << "in" << endl;
-                j++;
-            }
-            
-            res[i] = j;
-            
-        }
-        
-        return res;
-        
-    }
-    
+       
     int strStr(string haystack, string needle) {
-        
-        int n = haystack.size();
-        
-        vector <int> failure_array =  failure_function(needle);
-        
-        int m = needle.size();
-        
-        int j = 0;
-           
-        for(int i = 0; i < n; i++){
-            
-            while( j > 0 && haystack[i] != needle[j]){
-                j = failure_array[j - 1];
-            }
-            
-            if( haystack[i] == needle[j]){
-                j++;
-            }
-            
-            if( j == m ){
-                return i - m + 1;
-            }
-        }
-        
-        return -1;
+
+        return haystack.find(needle);
         
     }
 };
