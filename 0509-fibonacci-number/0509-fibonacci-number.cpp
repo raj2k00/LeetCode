87b60f08d251db0e1e342871dd209745165de903
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-04-23 17:06:57 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (94.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int fib(int n) {
+
+        if(n < 2) return n;
+        
         int firstNumber = 0;
         int secondNumber = 1;
-        
-        if(n == 0) return 0;
         
         for(int i = 2; i <= n; i++){
             int temp = (firstNumber + secondNumber) % 1000000007;
