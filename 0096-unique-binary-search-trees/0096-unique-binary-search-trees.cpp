6eba66a14cb95d74353b4623ebe05e3d8034e449
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-05-03 10:23:34 +0530

    Time: 0 ms (100.00%), Space: 6 MB (79.00%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long CalculateCoeff(int n,int k) //Function to calculate Ci(n.k)
+    {
+        long res=1;
+        if(k>n-k)
+            k=n-k;                    //Since Ci(n,k)=Ci(n,n-k), property of binomial coefficients
+        for(int i=0;i<k;i++)
+        {
+            res*=(n-i);
+            res/=(i+1);
+        }
+        return res;
+    }
+    int numTrees(int n) {
+        return CalculateCoeff(2*n,n)/(n+1);
+    }
+};
