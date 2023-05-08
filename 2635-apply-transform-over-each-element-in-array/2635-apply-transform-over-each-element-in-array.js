45421ea55585462831ac6e45b893dfcd690066e0
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-05-08 13:20:17 +0530

    Time: 55 ms (72.79%), Space: 41.9 MB (58.22%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const newArray = [];
+    
+    for(let i = 0; i < arr.length;  i++){
+        newArray.push(fn(arr[i],i));
+    }
+    
+    return newArray;
+};
