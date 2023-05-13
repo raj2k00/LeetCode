1c2fa05d44e0d75cd7d195764739204d33ddab53
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-05-13 09:36:10 +0530

    Time: 343 ms (84.96%) | Memory: 106.4 MB (34.30%) - LeetSync

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,18 +1,25 @@
 /**
  * @param {Function} fn
  */
- 
 function memoize(fn) {
-  const cache = {};
-  return function(...args) {
-    const key = JSON.stringify(args);
-    if (key in cache) {
-      return cache[key];
+
+    const cache = {};
+
+    return function(...args) {
+        
+        const key = JSON.stringify(args);
+
+        if(key in cache){
+            return cache[key];
+        }
+
+        else{
+            const result = fn(...args);
+            cache[key] = result;
+            return result;
+        }
+
     }
-    const functionOutput = fn(...args);
-    cache[key] = functionOutput;
-    return functionOutput;
-  }
 }
 
 
