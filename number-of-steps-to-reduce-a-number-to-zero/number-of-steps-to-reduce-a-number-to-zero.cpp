0e39a1b6e0698c865a1b50a96f54b640fa66b5df
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-05-20 12:46:01 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (92.94%) - LeetSync

--- number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
+++ number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
-
-        int count = 0;
-        if(num == 0) return count;
-
-        while(num != 0){
-            if(num % 2 == 0) { count++; num /= 2;} 
-            else {count++; num--;};
-        }
-
-        return count;
-
+        return num ? __builtin_popcount(num) + 31 - __builtin_clz(num) : 0;
     }
 };
