e9d346daeb5469883225340bd780bb7d88c97436
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-05-21 16:36:11 +0530

    Time: 73 ms (74.49%) | Memory: 45.3 MB (93.88%) - LeetSync

--- json-deep-equal/json-deep-equal.js
+++ json-deep-equal/json-deep-equal.js
@@ -4,19 +4,30 @@
  * @return {boolean}
  */
 
-function helper(key, value){
-    if(value && typeof value === 'object' && !Array.isArray(value) ){
-        return   Object.fromEntries(Object.entries(value).sort());
-    }else{
-        return value;
-    }
-}
-
 var areDeeplyEqual = function(o1, o2) {
+    if (o1 === o2) return true;
+    if (o1 === null || o2 === null) return false;
+    if (String(o1) !== String(o2)) return false;
 
-    const stringifiedObj1 = JSON.stringify(o1, helper)
-    const stringifiedObj2 = JSON.stringify(o2, helper)
+    if (typeof o1 !== 'object') {
+        return o1 === o2;
+    }
 
-    return  stringifiedObj1 === stringifiedObj2;
+    if (Array.isArray(o1)) {
+        if (o1.length !== o2.length) return false;
 
+        for (let i = 0; i < o1.length; i++) {
+            if (!areDeeplyEqual(o1[i], o2[i])) return false;
+        }
+
+        return true;
+    }
+
+    if (Object.keys(o1).length !== Object.keys(o2).length) return false;
+
+    for (const key in o1) {
+        if (!areDeeplyEqual(o1[key], o2[key])) return false;
+    }
+
+    return true;
 };
