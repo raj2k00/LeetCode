e1a80afabac996a5e7067860ffe80617f50dd58c
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-05-22 12:06:53 +0530

    Time: 44 ms (98.36%) | Memory: 41.7 MB (84.86%) - LeetSync

--- length-of-last-word/length-of-last-word.js
+++ length-of-last-word/length-of-last-word.js
@@ -4,13 +4,6 @@
  */
 var lengthOfLastWord = function(s) {
 
-    const s1 = s.split(" ");
+  return s.trim().split(" ").pop().length;
 
-    const s2 = s1.length - 1;
-    
-    for(let i = s2; i >= 0; i--){
-        if(s1[i] && s1[i].length > 0){
-            return s1[i].length
-        }
-    }
 };
