5d81cadfe00bc53ce5001684102421632e89b8a4
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-05-23 15:40:12 +0530

    Time: 3 ms (36.44%) | Memory: 5.9 MB (88.34%) - LeetSync

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,21 +1,10 @@
 class Solution {
 
-private:
-    unordered_map <string , int> mp;
-
 public:
     int uniquePaths(int m, int n) {
-        
-        string key = to_string(m) + "," + to_string(n);
-
-        if(mp.find(key) != mp.end()) return mp[key];
-
-        if( m == 1 && n == 1) return 1;
-        if( m == 0 || n == 0) return 0;
-
-        mp[key] = uniquePaths(m - 1, n) + uniquePaths(m , n - 1);
-
-        return mp[key];
-
+        long ans = 1;
+        for(int i = m+n-2, j = 1; i >= max(m, n); i--, j++) 
+            ans = (ans * i) / j;
+        return ans;
     }
 };
