4f4baa8f3b939b3267ceaadfd3652f1903b4676f
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-05-23 12:26:29 +0530

    Time: 51 ms (91.73%) | Memory: 41.6 MB (79.66%) - LeetSync

--- fibonacci-number/fibonacci-number.js
+++ fibonacci-number/fibonacci-number.js
@@ -9,7 +9,6 @@ var fib = function(n, memo = {}) {
     if(n == 0) return 0;
     if(n <= 2) return 1;
     
-    memo[n] =  fib(n - 1) + fib(n - 2);
+    memo[n] =  fib(n - 1, memo) + fib(n - 2,memo);
     return memo[n];
-
 };
