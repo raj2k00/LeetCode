be374ff1fc41a2b68b059f4bde976e0c25a26201
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-05-29 09:43:21 +0530

    Time: 103 ms (76.51%) | Memory: 51 MB (97.73%) - LeetSync

--- check-if-object-instance-of-class/check-if-object-instance-of-class.js
+++ check-if-object-instance-of-class/check-if-object-instance-of-class.js
@@ -3,14 +3,19 @@
  * @param {any} classFunction
  * @return {boolean}
  */
-var checkIfInstanceOf = function(obj, classFunction) {
-    
-    if (obj === null || obj === undefined || typeof classFunction !== "function") {
-        return false;
-    }
-    const inputObj = Object(obj)
-    
-    return inputObj instanceof classFunction;
+
+var checkIfInstanceOf = function (obj, classFunction) {
+  if (obj === null || obj === undefined || typeof classFunction !== "function")
+    return false;
+
+  let currPotentialPrototype = Object.getPrototypeOf(obj);
+
+  while (currPotentialPrototype !== null) {
+    if (currPotentialPrototype === classFunction.prototype) return true;
+    currPotentialPrototype = Object.getPrototypeOf(currPotentialPrototype);
+  }
+
+  return false;
 };
 
 /**
