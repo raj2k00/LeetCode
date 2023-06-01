c4ab137f999d7a7ceaa3ccbe78a4c87e17ef141f
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-06-01 11:44:56 +0530

    Time: 57 ms (72.56%) | Memory: 43.8 MB (65.33%) - LeetSync

---
+++ array-wrapper/array-wrapper.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ */
+var ArrayWrapper = function(nums) {
+    this.array = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+    let sum = 0;
+    for(const arr of this.array){
+        sum += arr;
+    }
+    return sum;
+}
+
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.array}]`
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
