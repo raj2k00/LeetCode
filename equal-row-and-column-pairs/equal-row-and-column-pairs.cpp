10ac831c5f3774ce143a76ae3e84afa91eedc5cb
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-06-14 09:51:07 +0530

    Time: 111 ms (62.82%) | Memory: 32.6 MB (54.35%) - LeetSync

--- equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-                map<vector<int>, int> hashmap;
-        int ans = 0;
-        int row = grid.size();
-        int col = grid[0].size();
-        for (int i=0; i<row; i++) {
-            hashmap[grid[i]]++;
-        }
-        for (int j=0; j<col; j++) {
-            vector<int> curr;
-            for (int i=0; i<row; i++) {
-                curr.emplace_back(grid[i][j]);
-            }
-            ans += hashmap[curr];
-        }
-        return ans;
+
+     int n = grid.size();
+     int count = 0;
+     map <vector <int> , int> hash;
+
+
+     for(int i = 0; i < n; i++){
+         hash[grid[i]]++;
+     }
+
+     for(int i = 0; i < n; i++){
+         vector <int> col;
+
+         for(int j = 0; j < n; j++){
+             col.emplace_back(grid[j][i]);
+         }
+
+         count += hash[col];
+     }
+
+
+
+    return count;
+
     }
 };
