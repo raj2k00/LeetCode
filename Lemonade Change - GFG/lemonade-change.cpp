c41589854cd63c7d91bbd5957a7485bd4aa4eef6
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-06-22 14:36:28 +0530

    Added solution - LeetHub

---
+++ Lemonade Change - GFG/lemonade-change.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool lemonadeChange(int N, vector<int> &bills) {
+        
+        
+        unordered_map <int,int> billCount = {{5,0},{10,0},{20,0}};
+
+        for(int i = 0; i < N ; i++){
+
+            if(bills[i] == 5){
+                billCount[5]++;
+            }
+            
+            if(bills[i] == 10){
+                if( billCount[5] > 0){
+                    billCount[5]--;
+                    billCount[10]++;
+                }else{
+                    return false;
+                }
+            }
+            
+            if(bills[i] == 20){
+                
+                if(billCount[10] > 0 && billCount[5] > 0){
+                    billCount[5]--;
+                    billCount[10]--;
+                    billCount[20]++;
+                }else if(billCount[5] >= 3){
+                    billCount[5]--;
+                    billCount[5]--;
+                    billCount[5]--;
+                    billCount[20]++;
+                }
+                else{
+                    return false;
+                }
+            }
+
+        }
+        
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<int> bills(N);
+        for (int i = 0; i < N; i++) cin >> bills[i];
+
+        Solution obj;
+        int ans = obj.lemonadeChange(N, bills);
+        if (ans)
+            cout << "True" << endl;
+        else
+            cout << "False" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
