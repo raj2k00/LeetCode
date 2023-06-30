602b21c4f1ab5f630f752d592cacbc5c6f012121
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-06-30 10:01:07 +0530

    Time: 12 ms (39.70%) | Memory: 10.1 MB (57.42%) - LeetSync

---
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    int getYDiff (vector <int> &a , vector <int> &b ){
+        return a[1] - b[1];
+    }
+
+    int getXDiff (vector <int> &a, vector <int> &b ){
+        return a[0] - b[0];
+    }
+
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+
+        int deltaY = getYDiff(coordinates[1], coordinates[0]); 
+        int deltaX = getXDiff(coordinates[1], coordinates[0]); 
+
+        for(int i = 2; i < coordinates.size(); i++){
+            if (deltaY * getXDiff(coordinates[i], coordinates[0])
+                != deltaX * getYDiff(coordinates[i], coordinates[0])) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
