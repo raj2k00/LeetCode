273f056c5f5ba353cf0ff024608ad2f0b5e96599
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-07-06 21:16:18 +0530

    Added solution - LeetHub

--- Quick Sort - GFG/quick-sort.cpp
+++ Quick Sort - GFG/quick-sort.cpp
@@ -19,57 +19,44 @@ class Solution
     //Function to sort an array using quick sort algorithm.
     void quickSort(int arr[], int low, int high)
     {
-        if(low < high){
-            int pivot = partition(arr,low,high);
-            // Lomuto Partition
-            // quickSort(arr,low,pivot-1);
-            // Hoare's Partition
-            quickSort(arr,low,pivot);
-            quickSort(arr,pivot+1,high);
+        // code here
+        if (low < high) {
+  
+        // pi is partitioning index, arr[p]
+        // is now at right place
+        int pi = partition(arr, low, high);
+  
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+            
         }
     }
     
     public:
     int partition (int arr[], int low, int high)
     {
-    //   Lomuto Partition
-    //   int i = low - 1;
-    //   int p = arr[high];
-       
-    //   for(int j = low; j <= high; j++){
-    //       if(arr[j] < p){
-    //           i++;
-    //           swap(arr[i], arr[j]);
-    //       }
-    //   }
-    //   swap(arr[i+1], arr[high]);
-       
-    //   return i+1;
-    
-    // Hoare's Partition
-    
-        int i = low - 1;
-        int j = high + 1;
-        int p = arr[low];
-        
-        while (true){
-            
-
-        do{
+        int pivot = arr[high];
+  
+    // Index of smaller element and indicates
+    // the right position of pivot found so far
+    int i = (low - 1);
+  
+    for (int j = low; j <= high - 1; j++) {
+  
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot) {
+  
+            // Increment index of smaller element
             i++;
-        }while(arr[i] < p);
-        
-        do{
-            j--;
-        }while(arr[j] > p);
-        
-        if(i >= j){
-            return j;
-        }
-        
-        swap(arr[i], arr[j]);
+            swap(arr[i], arr[j]);
         }
     }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+    }
+    
 };
 
 
