abe045a2c15f723fbacf4fd68980f4f9720ef7ec
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-07-09 19:24:40 +0530

    Time: 179 ms (78.62%) | Memory: 6.8 MB (62.26%) - LeetSync

--- substring-with-largest-variance/substring-with-largest-variance.cpp
+++ substring-with-largest-variance/substring-with-largest-variance.cpp
@@ -2,47 +2,48 @@ class Solution {
 public:
     int largestVariance(string s) {
 
-        vector <int> counter (26 , 0);
+        vector <int> counter (26, 0);
 
-        for (char ch : s){
+        for(char ch : s){
             counter[ch - 'a']++;
         }
 
         int globalMax = 0;
 
-        for(int i = 0 ;i < 26; i++){
+        for(int i = 0; i < 26; i++){
             for(int j = 0; j < 26; j++){
 
                 if(i == j || counter[i] == 0 || counter[j] == 0) continue;
 
-                char major = 'a' + i;
-                char minor = 'a' + j;
+                int major = i + 'a';
+                int minor = j + 'a';
                 int majorCount = 0;
                 int minorCount = 0;
 
                 int restMinor = counter[j];
 
                 for(char ch: s){
+
                     if(ch == major) majorCount++;
-                    
+
                     if(ch == minor){
                         minorCount++;
                         restMinor--;
                     }
 
-                    if(minorCount > 0){
-                        globalMax = max(globalMax, majorCount - minorCount);
-                    } 
-
                     if(majorCount < minorCount && restMinor > 0){
                         majorCount = 0;
                         minorCount = 0;
                     }
+
+                    if(minorCount > 0){
+                        globalMax = max(globalMax , majorCount - minorCount);
+                    }
+
                 }
             }
         }
 
         return globalMax;
-         
     }
 };
