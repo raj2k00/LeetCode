c96201cfa1a15d6f3266f0040718291c1cb54590
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-07-24 15:13:37 +0530

    Time: 2 ms (45.63%) | Memory: 6 MB (62.65%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
+
+    double binaryExp(double x, long long n){
+       if( n == 0) return 1;
+
+       if(n < 0){
+           n = -1 * n;
+           x = 1.0 / x;
+       }
+
+       double result = 1;
+
+       while(n){
+           
+           if(  n % 2 == 1){
+               result = result * x;
+               n -= 1;
+           }
+
+           x = x * x;
+           n = n / 2;
+       
+       }
+
+       return result;
+    }
+
     double myPow(double x, int n) {
-       return pow(x,n); 
+        return binaryExp(x, (long long)n);
     }
 };
