ff61da05d17e9ed55bcd595d66f28eb08474617e
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-08-04 16:39:13 +0530

    Time: 10 ms (59.87%) | Memory: 13.2 MB (38.95%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict;
+
+        vector<bool> dp(s.size() + 1);
+        
+        for (auto word : wordDict) {
+            dict.insert(word);
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            if (dict.count(s.substr(0, i + 1)))
+                dp[i] = true;
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && dict.count(s.substr(j + 1, i - j))) {
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.size() - 1];
+    }
+};
