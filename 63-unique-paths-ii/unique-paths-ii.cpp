33befef53a9a0066fc635261fa060d0d3426cb17
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-08-12 18:13:04 +0530

    Time: 5 ms (28.03%) | Memory: 7.9 MB (6.12%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
 
-    vector<vector<int>> memo;
-    vector<vector<int>> path;
-    int row, col;
-
-    int pathFinder(int m, int n){
-
-        if(memo[m][n]!= -1){
-            return memo[m][n];
-        }
-
-        if(m > row || n > col) return 0;
-        
-        if(path[m][n] == 1) return 0;
-
-        if(m == row && n == col) return 1;
-
-        memo[m][n] =  pathFinder(m+1, n) + pathFinder(m, n+1);
-
-        return memo[m][n];
-
-    }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        this -> path = obstacleGrid;
-        this -> row = m -1;
-        this -> col = n -1;
-        this -> memo = vector <vector<int>>(m+1, vector<int>(n+1, -1));
-        return pathFinder(0, 0);
 
-        
+        vector <vector<long>> grid (m+1, vector<long>(n+1, 0));
+
+        grid[0][1] = 1;
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if (obstacleGrid[i - 1][j - 1] != 1) {
+                    grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+                }
+            }
+        }
+
+
+        return grid[m][n];
+  
     }
 };
