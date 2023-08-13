efaf339fe8bbb4c99265bb34aae999368d5e6781
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-08-13 21:44:44 +0530

    Time: 135 ms (80.61%) | Memory: 84 MB (90.46%) - LeetSync

---
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool validPartition(vector<int>& nums) {
+        int n = nums.size();
+
+        vector <bool > dp (n +1, false);
+
+        dp[0] = true;
+
+        for(int i = 0; i < n; i++){
+            int dpIndex = i + 1;
+
+            if( i > 0 && nums[i] == nums[i - 1]){
+                dp[dpIndex] = (dp[dpIndex] | dp[dpIndex - 2]); 
+            }
+            
+            if (i > 1 && nums[i] == nums[i - 1] && nums[i] == nums[i - 2]) {
+                dp[dpIndex] = (dp[dpIndex] | dp[dpIndex - 3]);
+            }
+
+            if (i > 1 && nums[i] == nums[i - 1] + 1 && nums[i] == nums[i - 2] + 2) {
+                dp[dpIndex] = (dp[dpIndex] | dp[dpIndex - 3]);
+            }
+        }
+
+        return dp[n];
+    }
+};
