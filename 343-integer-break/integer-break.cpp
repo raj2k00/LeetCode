8eb9d6b573ef94358a693d1b52d71f6a2592f6b1
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-10-06 20:13:41 +0530

    Time: 2 ms (49.96%) | Memory: 6.6 MB (22.36%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        if (n == 2 || n == 3) return (n-1);
-        // Keep removing parts of size 3 while n is greater than 4
-        int res = 1;
-        while (n > 4)
-        {
-        n -= 3;
-        res *= 3; // Keep multiplying 3 to res
+
+        if (n <= 3) {
+            return n - 1;
         }
-        return (n * res);
+
+        if (n % 3 == 0) {
+            return pow(3, n / 3);
+        }
+        
+        if (n % 3 == 1) {
+            return pow(3, n / 3 - 1) * 4;
+        }
+        
+        return pow(3, n / 3) * 2;
     }
 };
