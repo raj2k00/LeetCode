067926447125dd29e1f249444dcfc2b4c7fa8c9e
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-12-02 16:51:33 +0530

    Time: 51 ms (65.85%) | Memory: 23 MB (54.98%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
 
-     unordered_map <char, int> input;
-     int ans;
+     vector <int> counts (26,0);
+     int ans = 0;
 
-     for(auto i : chars){
-         input[i]++;
+     for(auto c : chars){
+        counts[c - 'a']++;
      }
 
      for(auto i : words){
 
-        unordered_map <char, int> curr;
+       vector <int> wordCount (26,0);
+
         bool canForm = true;
 
         for(auto j : i){
-            curr[j]++;
+            wordCount[j - 'a']++;
         }
 
-        for(auto k : curr){
-            if (input[k.first] < k.second) {
+        for(int i = 0; i < 26; i++){
+            if (counts[i] < wordCount[i]) {
                 canForm = false;
             }
         }
