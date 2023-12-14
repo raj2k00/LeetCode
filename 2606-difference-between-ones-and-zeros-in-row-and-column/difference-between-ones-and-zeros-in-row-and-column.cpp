ba7028b6912b51a703eb11d9b2b4a8f3e9cb1422
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-12-14 09:50:26 +0530

    Time: 207 ms (35.67%) | Memory: 117.5 MB (65.73%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -8,31 +8,21 @@ public:
         vector <int> onesRow (row, 0);
         vector <int> onesCol (col, 0);
 
-        vector <int> zerosRow (row, 0);
-        vector <int> zerosCol (col, 0);
-
         vector <vector <int>> ans(row, vector<int>(col,0));
-
 
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
-                if(grid[i][j]){
-                    onesRow[i] = onesRow[i] + 1;
-                    onesCol[j] = onesCol[j] + 1;
-                }else{
-                    zerosRow[i] = zerosRow[i] + 1;
-                    zerosCol[j] = zerosCol[j] + 1;
-                }
+                onesRow[i] += grid[i][j];
+                onesCol[j] += grid[i][j];
             }
         }
 
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
-                ans[i][j] = onesRow[i] + onesCol[j] - zerosRow[i] - zerosCol[j];
+                ans[i][j] = 2 * onesRow[i] +  2 * onesCol[j] - row - col;
             }
         }
 
         return ans;
-        
     }
 };
