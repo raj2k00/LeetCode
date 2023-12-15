d4f6f29df730ffd691a5c80e2f87371e7a9026f5
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-12-15 09:20:58 +0530

    Time: 13 ms (29.07%) | Memory: 12.1 MB (6.34%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -2,31 +2,20 @@ class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
 
-        string ans = "";
-
         unordered_map <string, string> hash;
 
         int n = paths.size();
 
         if(n == 1) return paths[0][1];
 
-
         for(auto i : paths ){
             hash[i[0]] = i[1];
         }
 
         for(auto i: paths){
-            string s = hash[i[1]];
-            if( s.size() > 0){
-                continue;
-            }else{
-                return i[1];
-            }
+           if (hash[i[1]].size() == 0) return i[1];
         }
 
-        return ans;
-
-
-        
+        return "";
     }
 };
