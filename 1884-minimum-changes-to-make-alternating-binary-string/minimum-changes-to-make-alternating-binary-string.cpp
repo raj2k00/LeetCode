10842f775e33388521ac2fc629dfbc83ea37687b
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2023-12-24 11:48:03 +0530

    Time: 3 ms (78.34%) | Memory: 7.4 MB (24.33%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int oneCount = 0;
-        int zeroCount = 0;
-        string oneS =  s;
-        string zeroS =  s;
-
-
-        if(oneS[0] == '0') oneCount++;
+        int start0 = 0;
         
-        oneS[0] = '1';
-        char prev = '1';
-
-        for(int i = 1; i < s.size(); i++ ){
-            if(oneS[i] == prev ){
-                oneCount++;
-
-                if(prev == '0') oneS[i] = '1';
-                else oneS[i] = '0';
-          
+        for (int i = 0; i < s.size(); i++) {
+            if (i % 2 == 0) {
+                if (s[i] == '1') {
+                    start0++;
+                }
+            } else {
+                if (s[i] == '0') {
+                    start0++;
+                }
             }
-                  prev = oneS[i];
         }
-
-
-        if(zeroS[0] == '1') zeroCount++;
-        zeroS[0] = '0';
-        prev = '0';
-
-        for(int i = 1; i < s.size(); i++ ){
-            if(zeroS[i] == prev ){
-                zeroCount++;
-                
-                if(prev == '0') zeroS[i] = '1';
-                else zeroS[i] = '0';
-          
-            }
-                  prev = zeroS[i];
-        }
-
-        cout << oneCount << " " << zeroCount; 
-
-        return min(oneCount, zeroCount);
+        
+        return min(start0, (int) s.size() - start0);
     }
 };
