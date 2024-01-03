5c67b8c7de42a2cd3da49c88a6217162b3739607
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2024-01-03 20:49:30 +0530

    Time: 103 ms (26.52%) | Memory: 24.6 MB (39.73%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+
+        unordered_map <int, int> mp;
+        int ans = 0;
+        int n = bank.size();
+
+        for(int i = 0; i < n; i++){
+            for(char c : bank[i]){
+                if(c == '1'){
+                    mp[i]++;
+                }
+            }
+        }
+
+        int i = 0;
+        int j = 1;
+        bool hasMultiplied = false;
+        bool hasSecurity = false;
+
+        while(i < j && i < n - 1 && j < n){
+            if(mp[i] && mp[j]){
+                ans += mp[i] * mp[j];
+                i = j;
+                j++;
+            }else if(mp[j] == 0){
+                j++;
+            }else if(mp[i] == 0){
+                i++;
+                j++;
+            }else if(mp[i] && mp[j] == 0){
+                j++;
+            }else if(mp[j] && mp[i] == 0){
+                i++;
+                j++;
+            }
+        }
+        return ans;
+
+        
+    }
+};
