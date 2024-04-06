720bd593dae23e3989e1849667d2464590a99767
Mohanraj G <68595641+raj2k00@users.noreply.github.com>
2024-04-06 22:03:38 +0530

    Time: 25 ms (31.31%) | Memory: 13.1 MB (44.22%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,41 @@
+
+class Solution {
+public:
+    std::string minRemoveToMakeValid(std::string s) {
+        int leftCount = 0;
+        int rightCount = 0;
+        std::stack<char> stack;
+
+        // Pass 1
+        for (char ch : s) {
+            if (ch == '(') {
+                leftCount++;
+            } else if (ch == ')') {
+                rightCount++;
+            }
+            if (rightCount > leftCount) {
+                rightCount--;
+                continue;
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        std::string result = "";
+        
+        // Pass 2
+        while (!stack.empty()) {
+            char currentChar = stack.top();
+            stack.pop();
+            if (leftCount > rightCount && currentChar == '(') {
+                leftCount--;
+            } else {
+                result += currentChar;
+            }
+        }
+
+        // Reverse the result string
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
